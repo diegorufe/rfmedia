97f4f0b6fbce2de0083b5eeb5dd38633365ef581
diegorufe <diegorufe@gmail.com>
2019-03-18 06:22:44 +0100

    Added authentication controller for rest

--- RFRest/src/main/java/com/RFRest/constants/IConstantsRest.java
+++ RFRest/src/main/java/com/RFRest/constants/IConstantsRest.java
@@ -11,4 +11,6 @@ public interface IConstantsRest {
 	public static String URL_FIND = "/find";
 	public static String URL_INSERT = "/insert";
 	public static String URL_DELETE = "/delete";
+	public static final String REST_URL_CROSS_ORIGIN = "*";
+	public static final int MAX_AGE_CROSS_ORIGIN = 3600;
 }
