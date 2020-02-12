7c78cacc2c62673709374d65418158c7f074babd
diegorufe <diegorufe@gmail.com>
2020-02-12 21:27:18 +0100

    Remove testkiller.pdf and added jar maven for RFERP

--- RFERP/src/main/java/com/RFERP/RFERPApplication.java
+++ RFERP/src/main/java/com/RFERP/RFERPApplication.java
@@ -2,9 +2,16 @@ package com.RFERP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RFERPApplication {
+public class RFERPApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RFERPApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RFERPApplication.class, args);
--- RFRestCoreERP/src/main/java/com/RFRestCoreERP/RFRestCoreERPApp.java
+++ RFRestCoreERP/src/main/java/com/RFRestCoreERP/RFRestCoreERPApp.java
@@ -2,9 +2,10 @@ package com.RFRestCoreERP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RFRestCoreERPApp {
+public class RFRestCoreERPApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RFRestCoreERPApp.class, args);
